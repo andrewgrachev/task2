8c57ae2d6a9d6923eb12b1c8fd6c116a3c3e0747
andrew <dreamwuzker@gmail.com>
2017-08-21 01:25:44 +0500

    diagram(-)

--- main.py
+++ main.py
@@ -1,70 +1,87 @@
 import os
 import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+#from skimage import data, io, filters
 
-# import matplotlib.pyplot as plt
-# from skimage import data
+
+Dir = input("Dir: ")
+
+try:
+    K = int(input('inpit K:'))
+except (SyntaxError, ValueError):
+    K = 16
+
+try:
+    S = tuple(int(x.strip()) for x in input('Input S:').split(','))
+except (SyntaxError, ValueError):
+    S = 128, 128
+
 
 def reshape_array(inputArray):
-    result = np.zeros((128, 128, 3))
+    result = np.zeros((size[1], size[0], 3))
 
     for i, ar in enumerate(inputArray):
         for j, item in enumerate(inputArray[i]):
-            result[i,j,0] = item
-            result[i,j,1] = item
-            result[i,j,2] = item
+            result[i, j, 0] = item
+            result[i, j, 1] = item
+            result[i, j, 2] = item
 
     return result
 
-S = 128, 128
-K = 16
-
-Dir = input("Dir: ")
-#K = int(input('inpit K:'))
-#S = tuple(int(x.strip()) for x in input('Input S:').split(','))
-
-# /home/andrew/okas/task2/101_ObjectCategories
+allimages = []
 os.chdir(Dir)
 choices = []
-
 directory = os.listdir(Dir)
-choices = np.random.choice(directory, K)
+choices = np.random.choice(directory, K, replace=False)
+dIrects = []
 
 for dIrect in choices:
     os.chdir(Dir + "/" + dIrect)
+    # dIrect = title for subplot
+    dIrects.append(dIrect)
     allfiles = os.listdir(os.getcwd())
     imlist = [filename for filename in allfiles if filename[-4:] in [".jpg", ".JPG"]]
     size = S
-
     arr = np.zeros((size[1], size[0], 3), np.float)
-
     images = []
-    print("========================")
+
     for fname in imlist:
-        print(fname)
         image = Image.open(fname)
-        print(image)
         resizedImage = image.resize(size)
-        print(type(resizedImage))
         imageArrayData = np.array(resizedImage)
-        print(type(imageArrayData))
-        print(imageArrayData.shape)
-        #print(imageArrayData.reshape(128,128,3).shape)
 
-        if imageArrayData.shape == (128, 128):
+        if imageArrayData.shape == (size[1], size[0]):
             imageArrayData = reshape_array(imageArrayData)
-            print("EBUSHKI VOROBUSHKI")
 
-        #imageArrayDataReshaped = np.reshape(imageArrayData, (128,128,3))
-        #видимо надо тут проверить если это 2д - делать ее в 3д. или
-        #момент кое что чекну
-        print(imageArrayData.shape)
-        if imageArrayData.shape == (128, 128, 3):
+        if imageArrayData.shape == (size[1], size[0], 3):
             images.append(imageArrayData)
-        print("==================")
 
     arr = np.array(np.mean(images, axis=(0)), dtype=np.uint8)
     out = Image.fromarray(arr)
-#    out.save('average.png')
-    out.show()
+    allimages.append(arr)
+    #out.show()
 
+
+
+def plot_figures(figures, nrows = 4, ncols=4):
+    """Plot a dictionary of figures.
+
+    Parameters
+    ----------
+    figures : <title, figure> dictionary
+    ncols : number of columns of subplots wanted in the display
+    nrows : number of rows of subplots wanted in the figure
+    """
+
+    fig, axeslist = plt.subplots(ncols=ncols, nrows=nrows)
+    for ind,title in zip(range(len(figures)), figures):
+        axeslist.ravel()[ind].imshow(figures[title])
+        axeslist.ravel()[ind].set_title(title)
+        axeslist.ravel()[ind].set_axis_off()
+    plt.tight_layout() # optional
+
+
+figures = {dIrects:allimages for dIrects in range(K)}
+plot_figures(figures, 4, 4)
